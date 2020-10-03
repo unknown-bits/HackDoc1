84b943e000307812c1d4bc07de20171dd9c18e04
unknown-bits <67140092+unknown-bits@users.noreply.github.com>
2020-10-03 14:28:56 +0530

    Revert "added boolean array to store status "

--- models/hospital-model.js
+++ models/hospital-model.js
@@ -9,11 +9,11 @@ const hospitalSchema = new Schema({
     contact1:{ type:String, default:"0000000000" ,minlength:10},
     contact2:{ type:String, default:"0000000000"},
     facility:{type:booleanArray, default: [0,0,0,0,0]},
-    bedavail1:{type:booleanArray, default: [0,0,0]},
-    bedavail2:{type:booleanArray, default: [0,0,0]},
+    bedavail1:{type:Array, default: [0,0,0]},
+    bedavail2:{type:Array, default: [0,0,0]},
     docavail:{type:booleanArray, default:  [0,0,0,0,0]},
-    bloodavail1:{type:booleanArray, default: [0,0,0,0]},
-    bloodavail2:{type:booleanArray, default: [0,0,0,0]}
+    bloodavail1:{type:Array, default: [0,0,0,0]},
+    bloodavail2:{type:Array, default: [0,0,0,0]}
 });
 
 const Hospital = mongoose.model('hospital',hospitalSchema);
